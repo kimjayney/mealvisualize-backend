1664c50f779a138d8fa1431f0734b8410a10f1c7
jayneykim <gim515@naver.com>
2024-01-01 17:45:39 +0900

    Migration

---
+++ d1migrationsfolder/0000_mealdbmigrate.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS Mealdb (
+    MealId INTEGER PRIMARY KEY AUTOINCREMENT, 
+    NAME VARCHAR(50),
+    Location VARCHAR(15),
+    Mealtype VARCHAR(15),    
+    Feel     VARCHAR(25),
+    Price    INTEGER,
+    Rating   FLOAT,
+    Address  VARCHAR(50),
+    REVIEW_CONTENT  TEXT,
+    Lat       Decimal(8,6),
+    Lng       Decimal(9,6),
+    ADMIN_OK BOOLEAN
+);
+
+ALTER TABLE Mealdb
+ADD UUID varchar(36);
